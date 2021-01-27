03180b9013d85d23600160c5bc968da6be373e37
nigh7sh4de <plotnikd@gmail.com>
2021-01-27 00:22:32 -0500

    Typescript safety

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,36 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import { SafeAreaView, Text, View, } from 'react-native';
-import { NativeRouter as Router, Switch, Route } from 'react-router-native'
-import { createStore, applyMiddleware } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
+import {SafeAreaView, Text, View} from 'react-native';
+import {NativeRouter as Router, Switch, Route} from 'react-router-native';
+import {createStore, applyMiddleware} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
 import AsyncStorage from '@react-native-community/async-storage';
-import { Provider } from 'react-redux'
-import Logger from 'redux-logger'
-import Thunk from 'redux-thunk'
-import reducers from 'src/redux';
+import {Provider} from 'react-redux';
+import Logger from 'redux-logger';
+import Thunk from 'redux-thunk';
+
+import {rootReducer, RootState, RootActions} from 'src/redux';
 
 const persistConfig = {
   key: 'mycity',
   storage: AsyncStorage,
-  blacklist: ['firestore'],
-}
+};
 
 const store = createStore(
-  persistReducer(persistConfig, reducers),
-  applyMiddleware(Thunk, Logger)
-)
-const persistor = persistStore(store)
+  persistReducer<RootState, RootActions>(persistConfig, rootReducer),
+  applyMiddleware(Thunk, Logger),
+);
+const persistor = persistStore(store as any);
 
 declare const global: {HermesInternal: null | {}};
 
@@ -39,18 +29,20 @@ const helloWorld = () => {
     <View>
       <Text>Hello World :D</Text>
     </View>
-  )
+  );
 };
 
 const App = () => {
   return (
-    <View style={{ flexGrow: 1 }}>
+    <View style={{flexGrow: 1}}>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <SafeAreaView style={{ flex: 1 }}>
+          <SafeAreaView style={{flex: 1}}>
             <Router>
-              <View style={{ flex: 1 }}>
-                <Route path="/" render={helloWorld} />
+              <View style={{flex: 1}}>
+                <Switch>
+                  <Route exact path="/" render={helloWorld} />
+                </Switch>
               </View>
             </Router>
           </SafeAreaView>
